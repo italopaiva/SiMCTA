19b7b08c50ddf13cf2b5ef63224a4f333a6a6b63
italopaiva <italosensei@hotmail.com>
2015-09-11 17:50:53 -0300

    Issue #1 - Creating default view class

---
+++ src/view/View.java
@@ -0,0 +1,72 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class View extends JFrame {
+	
+	protected JMenuBar menuBar;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					View frame = new View();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	protected void instantiateMenuBar(){
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu courseMenu = new JMenu("Cursos");
+		menuBar.add(courseMenu);
+		
+		JMenuItem registerCourse = new JMenuItem("Cadastrar Curso");
+		registerCourse.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				dispose();
+				
+				NewCourse newCourseFrame = new NewCourse();
+				newCourseFrame.setVisible(true);
+			}
+		});
+		courseMenu.add(registerCourse);
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public View(){
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		
+		instantiateMenuBar();
+	}
+}
