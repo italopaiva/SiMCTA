12acad81208f968a889ea8c99b22c06505ed5b55
EmilieMorais <emilie.mo@hotmail.com>
2015-10-10 18:09:04 -0300

    Issue #17 - Creating constructor for Student

---
+++ src/exception/StudentException.java
@@ -0,0 +1,9 @@
+package exception;
+
+public class StudentException extends Exception {
+
+	public StudentException(String message){
+		super(message);
+	}
+	
+}
--- src/model/Student.java
+++ src/model/Student.java
@@ -1,13 +1,13 @@
 package model;
 
-import java.util.Date;
-
+import exception.StudentException;
 import model.datatype.CPF;
 import model.datatype.RG;
 import model.datatype.Address;
 import model.datatype.Phone;
+import model.datatype.Date;
 
-public class Student {
+public class Student extends Model{
 	
 	private String studentName;
 	private CPF studentCpf;
@@ -18,4 +18,63 @@ public class Student {
 	private Phone secondaryPhone;
 	private String motherName;
 	private String fatherName;
+	
+	
+	public Student(String studentName, CPF studentCpf, RG studentRg,
+			Date birthdate, Address address, Phone principalPhone,
+			Phone secondaryPhone, String motherName, String fatherName) throws StudentException {
+			setStudentName(studentName);
+			setStudentCpf(studentCpf);
+			setStudentRg(studentRg);
+			setBirthdate(birthdate);
+			setAddress(address);
+			setPrincipalPhone(principalPhone);
+			setSecondaryPhone(secondaryPhone);
+			setMotherName(motherName);
+			setFatherName(fatherName);
+	}
+	
+	public Student(Date birth) throws StudentException{
+		setBirthdate(birth);
+	}
+
+	private void setFatherName(String fatherName) {
+		
+	}
+
+	private void setMotherName(String motherName) {
+		
+	}
+
+	private void setSecondaryPhone(Phone secondaryPhone) {
+		
+	}
+
+	private void setPrincipalPhone(Phone principalPhone) {
+	
+		
+	}
+
+	private void setAddress(Address address) {
+		
+		
+	}
+
+	private void setStudentRg(RG studentRg) {
+		
+	}
+
+	private void setStudentCpf(CPF studentCpf2) {
+		
+	}
+
+	private void setStudentName(String studentName2) {
+		
+	}
+
+	private void setBirthdate(Date birthdate) throws StudentException{
+
+		
+	}
+	
 }
