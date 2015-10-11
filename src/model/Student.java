31866e5da50286031df2c5948aa1c81b247b44fd
Italo Paiva <italosensei@hotmail.com>
2015-10-11 15:48:49 -0300

    Issue #17 - Testing student, mother and father name on Student model

--- src/model/Student.java
+++ src/model/Student.java
@@ -9,6 +9,13 @@ import model.datatype.Date;
 
 public class Student extends Model{
 	
+	private static final String NAMES_MUST_BE_ALPHA_CHARACTERS = "O nomes devem conter apenas caractares alfabéticos.";
+	private static final String PHONE_CANT_BE_NULL = "Os telefones não podem ficar em branco.";
+	private static final String ADDRESS_CANT_BE_NULL = "O endereço não pode estar em braco.";
+	private static final String BIRTHDATE_CANT_BE_NULL = "A data de aniversário não pode estar em branco.";
+	private static final String RG_CANT_BE_NULL = "O RG não pode estar em branco.";
+	private static final String CPF_CANT_BE_NULL = "O CPF não pode estar em branco.";
+	
 	private String studentName;
 	private CPF studentCpf;
 	private RG studentRg;
@@ -19,62 +26,158 @@ public class Student extends Model{
 	private String motherName;
 	private String fatherName;
 	
-	
 	public Student(String studentName, CPF studentCpf, RG studentRg,
 			Date birthdate, Address address, Phone principalPhone,
 			Phone secondaryPhone, String motherName, String fatherName) throws StudentException {
-			setStudentName(studentName);
-			setStudentCpf(studentCpf);
-			setStudentRg(studentRg);
-			setBirthdate(birthdate);
-			setAddress(address);
-			setPrincipalPhone(principalPhone);
-			setSecondaryPhone(secondaryPhone);
-			setMotherName(motherName);
-			setFatherName(fatherName);
+			
+		setStudentName(studentName);
+		setStudentCpf(studentCpf);
+		setStudentRg(studentRg);
+		setBirthdate(birthdate);
+		setAddress(address);
+		setPrincipalPhone(principalPhone);
+		setSecondaryPhone(secondaryPhone);
+		setMotherName(motherName);
+		setFatherName(fatherName);
 	}
 	
 	public Student(Date birth) throws StudentException{
 		setBirthdate(birth);
 	}
 
-	private void setFatherName(String fatherName) {
+	private void setFatherName(String fatherName) throws StudentException {
 		
+		if(fatherName != null && fatherName != ""){
+			
+			if(containsOnlyLettersAndSpaces(fatherName)){
+				
+				this.fatherName = fatherName;
+			}
+			else{
+				throw new StudentException(NAMES_MUST_BE_ALPHA_CHARACTERS);
+			}
+		}
+		else{
+			this.fatherName = "";
+		}
 	}
 
-	private void setMotherName(String motherName) {
+	private void setMotherName(String motherName) throws StudentException {
 		
+		if(containsOnlyLettersAndSpaces(motherName)){
+			
+			this.motherName = motherName;
+		}
+		else{
+			throw new StudentException(NAMES_MUST_BE_ALPHA_CHARACTERS);
+		}
 	}
-
-	private void setSecondaryPhone(Phone secondaryPhone) {
-		
-	}
-
-	private void setPrincipalPhone(Phone principalPhone) {
 	
+	private void setStudentName(String studentName) throws StudentException{
 		
+		if(containsOnlyLettersAndSpaces(studentName)){
+			this.studentName = studentName;
+		}
+		else{
+			throw new StudentException(NAMES_MUST_BE_ALPHA_CHARACTERS);
+		}
 	}
 
-	private void setAddress(Address address) {
+	private void setSecondaryPhone(Phone secondaryPhone) throws StudentException{
 		
-		
+		if(secondaryPhone != null){
+			this.principalPhone = secondaryPhone;
+		}
+		else{
+			throw new StudentException(PHONE_CANT_BE_NULL);
+		}
 	}
 
-	private void setStudentRg(RG studentRg) {
+	private void setPrincipalPhone(Phone principalPhone) throws StudentException{
 		
+		if(principalPhone != null){
+			this.principalPhone = principalPhone;
+		}
+		else{
+			throw new StudentException(PHONE_CANT_BE_NULL);
+		}
 	}
 
-	private void setStudentCpf(CPF studentCpf2) {
+	private void setAddress(Address address) throws StudentException{
 		
+		if(address != null){
+			this.address = address;
+		}
+		else{
+			throw new StudentException(PHONE_CANT_BE_NULL);
+		}
 	}
 
-	private void setStudentName(String studentName2) {
+	private void setStudentRg(RG studentRg) throws StudentException{
 		
+		if(studentRg != null){
+			this.studentRg = studentRg;
+		}
+		else{
+			throw new StudentException(RG_CANT_BE_NULL);
+		}
+	}
+
+	private void setStudentCpf(CPF studentCpf) throws StudentException{
+		
+		if(studentCpf != null){
+			this.studentCpf = studentCpf;
+		}
+		else{
+			throw new StudentException(CPF_CANT_BE_NULL);
+		}
 	}
 
 	private void setBirthdate(Date birthdate) throws StudentException{
-
 		
+		if(birthdate != null){
+			this.birthdate = birthdate;
+		}
+		else{
+			throw new StudentException(BIRTHDATE_CANT_BE_NULL);
+		}
 	}
-	
+
+	public String getStudentName(){
+		return studentName;
+	}
+
+	public CPF getStudentCpf(){
+		return studentCpf;
+	}
+
+	public RG getStudentRg(){
+		return studentRg;
+	}
+
+	public Date getBirthdate(){
+		return birthdate;
+	}
+
+	public Address getAddress(){
+		return address;
+	}
+
+	public Phone getPrincipalPhone(){
+		return principalPhone;
+	}
+
+	public Phone getSecondaryPhone(){
+		return secondaryPhone;
+	}
+
+	public String getMotherName(){
+		return motherName;
+	}
+
+	public String getFatherName(){
+		return fatherName;
+	}
 }
+
+	
--- test/modelTest/StudentTest.java
+++ test/modelTest/StudentTest.java
@@ -2,14 +2,17 @@ package modelTest;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import exception.AddressException;
 import exception.CPFException;
 import exception.DateException;
 import exception.PhoneException;
 import exception.RGException;
 import exception.StudentException;
 import model.Student;
+import model.datatype.Address;
 import model.datatype.CPF;
 import model.datatype.Phone;
 import model.datatype.RG;
@@ -18,36 +21,157 @@ import model.datatype.Date;
 public class StudentTest {
 	
 	Student student;
+	Date date;
+	Address address;
+	Phone phone1;
+	Phone phone2;
+	CPF cpf;
+	RG rg;
 	
-	public void testIfCreatesAStudent() throws PhoneException, CPFException, RGException, StudentException, DateException{
+	@Before
+	public void setUp() throws PhoneException, CPFException, RGException, StudentException, DateException, AddressException{
 		
-		
-		Date date = new Date(1986,05,11);
-		//Address address = new Address();
-		Phone phone1 = new Phone("61","83265622");
-		Phone phone2 = new Phone("61","32551111");
-		CPF cpf = new CPF("87654321124");
-		RG rg = new RG("8598298", "SSP", "DF");
-		
-		student = new Student("Jacó Mario Souza",cpf, rg, date, null, phone1, phone2, 
-						      "Milene Souza Medeiros", "Mário Souza Filho");
+		date = new Date(05, 06, 1996);
+		address = new Address("Rua 3 ", "6B", "", "72323411", "Brasília");
+		phone1 = new Phone("61","83265622");
+		phone2 = new Phone("61","32551111");
+		cpf = new CPF("51464638403");
+		rg = new RG("8598298", "SSP", "DF");
 	}
 	
-	/**
-	 * Birthdate tests
-	 * @throws StudentException
-	 */
+	/** Tests for student name */
 	@Test
-	public void testBirthDateValid() throws StudentException{
+	public void testValidStudentName(){
 		
-				
+		String validStudentName = "Jacó Mario Souza";
+		
+		try {
+			student = new Student(validStudentName, cpf, rg, date, address, phone1, phone2, 
+							      "Milene Souza Medeiros", "Mário Souza Filho");
+			assertEquals(validStudentName, student.getStudentName());
+		} catch (StudentException e){
+			fail("Should not throw this exception: "+e.getMessage());
+		}
 	}
 	
 	@Test(expected = StudentException.class)
-	public void testBirthDateInvalid() throws StudentException{
+	public void testEmptyStudentName() throws StudentException{
 		
-		Date birthdate = null;
-		student = new Student(birthdate);
+		String validStudentName = "";
+		
+		student = new Student(validStudentName, cpf, rg, date, address, phone1, phone2, 
+							      "Milene Souza Medeiros", "Mário Souza Filho");
 	}
 	
+	@Test(expected = StudentException.class)
+	public void testNullStudentName() throws StudentException{
+		
+		String validStudentName = null;
+		
+		student = new Student(validStudentName, cpf, rg, date, address, phone1, phone2, 
+							      "Milene Souza Medeiros", "Mário Souza Filho");
+	}
+	
+	@Test(expected = StudentException.class)
+	public void testNonLettersStudentName() throws StudentException{
+		
+		String validStudentName = "João 123";
+		
+		student = new Student(validStudentName, cpf, rg, date, address, phone1, phone2, 
+							      "Milene Souza Medeiros", "Mário Souza Filho");
+	}
+	
+	/** Tests for mother name*/
+	@Test
+	public void testValidMotherName(){
+		
+		String validMotherName = "Milene Souza Medeiros";
+		
+		try {
+			student = new Student("Jacó Mario Souza", cpf, rg, date, address, phone1, phone2, 
+							      validMotherName, "Mário Souza Filho");
+			assertEquals(validMotherName, student.getMotherName());
+		} catch (StudentException e){
+			fail("Should not throw this exception: "+e.getMessage());
+		}
+	}
+	
+	@Test(expected = StudentException.class)
+	public void testEmptyMotherName() throws StudentException{
+		
+		String invalidMotherName = "";
+		
+		student = new Student("Jacó Mario Souza", cpf, rg, date, address, phone1, phone2, 
+						      invalidMotherName, "Mário Souza Filho");
+	}
+
+	@Test(expected = StudentException.class)
+	public void testNullMotherName() throws StudentException{
+		
+		String invalidMotherName = null;
+		
+		student = new Student("Jacó Mario Souza", cpf, rg, date, address, phone1, phone2, 
+						      invalidMotherName, "Mário Souza Filho");
+	}
+	
+	@Test(expected = StudentException.class)
+	public void testNonLettersMotherName() throws StudentException{
+		
+		String invalidMotherName = "Milene 124";
+		
+		student = new Student("Jacó Mario Souza", cpf, rg, date, address, phone1, phone2, 
+						      invalidMotherName, "Mário Souza Filho");
+	}
+	
+	/** Tests for father name*/
+	@Test
+	public void testValidFatherName(){
+		
+		String validFatherName = "Mário Souza Filho";
+		
+		try {
+			student = new Student("Jacó Mario Souza", cpf, rg, date, address, phone1, phone2, 
+								  "Milene Souza Medeiros", validFatherName);
+			assertEquals(validFatherName, student.getFatherName());
+		} catch (StudentException e){
+			fail("Should not throw this exception: "+e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testValidEmptyFatherName(){
+		
+		String validFatherName = "";
+		
+		try {
+			student = new Student("Jacó Mario Souza", cpf, rg, date, address, phone1, phone2, 
+								  "Milene Souza Medeiros", validFatherName);
+			assertEquals("", student.getFatherName());
+		} catch (StudentException e){
+			fail("Should not throw this exception: "+e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testValidNullFatherName(){
+		
+		String validFatherName = null;
+		
+		try {
+			student = new Student("Jacó Mario Souza", cpf, rg, date, address, phone1, phone2, 
+								  "Milene Souza Medeiros", validFatherName);
+			assertEquals("", student.getFatherName());
+		} catch (StudentException e){
+			fail("Should not throw this exception: "+e.getMessage());
+		}
+	}
+	
+	@Test(expected = StudentException.class)
+	public void testNonLettersFatherName() throws StudentException{
+		
+		String invalidFatherName = "Mário 123";
+		
+		student = new Student("Jacó Mario Souza", cpf, rg, date, address, phone1, phone2, 
+							  "Milene Souza Medeiros", invalidFatherName);
+	}
 }
