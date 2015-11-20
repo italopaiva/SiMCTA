e62acb11dacc2eda7a5b36f9c81fb023cb3005cf
Italo Paiva <italosensei@hotmail.com>
2015-11-20 13:26:23 -0200

    Issue #30 - Finalizing tests of Service model

--- test/modelTest/ServiceTest.java
+++ test/modelTest/ServiceTest.java
@@ -4,8 +4,11 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 
+import model.Course;
+import model.Package;
 import model.Payment;
 import model.Service;
+import model.ServiceItem;
 import model.Student;
 import model.datatype.Address;
 import model.datatype.CPF;
@@ -18,6 +21,7 @@ import org.junit.Test;
 
 import exception.AddressException;
 import exception.CPFException;
+import exception.CourseException;
 import exception.DateException;
 import exception.PaymentException;
 import exception.PersonException;
@@ -31,6 +35,9 @@ public class ServiceTest {
 	private Service service;
 	private Student student;
 	private Payment payment;
+	private Course course1;
+	private Course course2;
+	private Package package1;
 	
 	private Date contractDate;
 	
@@ -43,8 +50,7 @@ public class ServiceTest {
 	private String email;
 	
 	@Before
-	public void setUp() throws DateException, AddressException, PhoneException,
-						CPFException, RGException, StudentException, PersonException, PaymentException{
+	public void setUp() throws Exception{
 		
 		date = new Date(05, 06, 1996);
 		address = new Address("Rua 3 ", "6B", "", "72323411", "Brasília");
@@ -60,6 +66,12 @@ public class ServiceTest {
 		contractDate = new Date(10, 10, 2015);
 		
 		payment = new Payment(2, 1, 1, 2);
+		
+		course1 = new Course("Aplicação de portas", "Aplica portas.", 5, 150000);
+		course2 = new Course("Aplicação de janelas", "Aplica janelas.", 3, 250000);
+		package1 = new Package(1, "Aplicação top", 300000);
+		package1.addServiceItem(course1);
+		package1.addServiceItem(course2);
 	}
 	
 	/** Tests for constructors */
@@ -174,7 +186,141 @@ public class ServiceTest {
 		service = new Service(1, null, contractDate, payment);
 	}
 	
-	/** Tests for courses */
+	/** Test for addItem() */
+	@Test
+	public void testIfAddValidItem(){
 		
+		try{
+						
+			service = new Service(1, student, contractDate, payment);
+			service.addItem(course1);
+			
+			assertEquals(service.getItens().get(0), course1);
+		}
+		catch(ServiceException e){
+			fail("Should not throw this exception:" + e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testIfAddValidPackageItem(){
+		
+		try{
+						
+			service = new Service(1, student, contractDate, payment);
+			service.addItem(package1);
+			
+			assertEquals(service.getItens().get(0), package1);
+		}
+		catch(ServiceException e){
+			fail("Should not throw this exception:" + e.getMessage());
+		}
+	}
+	
+	@Test(expected = ServiceException.class)
+	public void testIfAddInvalidNullItem() throws ServiceException{
+						
+		service = new Service(1, student, contractDate, payment);
+		service.addItem(null);
+	}
+	
+	/** Tests for get methods*/
+	@Test
+	public void testIfGetServiceValueWithPackage(){
+		
+		try{
+						
+			service = new Service(1, student, contractDate, payment);
+			service.addItem(package1);
+			
+			assertEquals(new Integer(300000), service.getTotalValue());
+		}
+		catch(ServiceException e){
+			fail("Should not throw this exception:" + e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testIfGetServiceValueWithCourses(){
+		
+		try{
+						
+			service = new Service(1, student, contractDate, payment);
+			service.addItem(course1);
+			service.addItem(course2);
+			
+			assertEquals(new Integer(400000), service.getTotalValue());
+		}
+		catch(ServiceException e){
+			fail("Should not throw this exception:" + e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testIfGetFormattedServiceValue(){
+		
+		try{
+						
+			service = new Service(1, student, contractDate, payment);
+			service.addItem(course1);
+			service.addItem(course2);
+			
+			assertEquals("R$ 4000,00", service.getTotalValueFormatted());
+		}
+		catch(ServiceException e){
+			fail("Should not throw this exception:" + e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testIfGetInstallmentsValue(){
+		
+		try{
+						
+			service = new Service(1, student, contractDate, payment);
+			service.addItem(course1);
+			service.addItem(course2);
+			
+			assertEquals("R$ 2000,00", service.getInstallmentsValue());
+		}
+		catch(ServiceException e){
+			fail("Should not throw this exception:" + e.getMessage());
+		}
+	}
+	
+	/** Tests for courses */
+	
+	@Test
+	public void testIfGetCoursesOfService(){
+		
+		try{
+						
+			service = new Service(1, student, contractDate, payment);
+			service.addItem(course1);
+			service.addItem(package1);
+			
+			assertEquals(course1, service.getCourses().get(0));
+		}
+		catch(ServiceException e){
+			fail("Should not throw this exception:" + e.getMessage());
+		}
+	}
+	
 	/** Tests for packages */
+	
+	@Test
+	public void testIfGetPackagesOfService(){
+		
+		try{
+						
+			service = new Service(1, student, contractDate, payment);
+			service.addItem(course1);
+			service.addItem(package1);
+			
+			assertEquals(package1, service.getPackages().get(0));
+		}
+		catch(ServiceException e){
+			fail("Should not throw this exception:" + e.getMessage());
+		}
+	}
 }
