2ae4e54ed6cc6b63244447bde027c048afa9a401
Edson Gomes <edinhogomes@gmail.com>
2015-11-22 04:19:35 -0200

    Issue #34 - Implements methods in ClassDAO to search a Class.

--- src/dao/ClassDAO.java
+++ src/dao/ClassDAO.java
@@ -2,12 +2,21 @@ package dao;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
+import org.hamcrest.core.IsNull;
+
+import controller.CourseController;
+import controller.TeacherController;
+import exception.CPFException;
 import exception.ClassException;
 import exception.DateException;
+import exception.PersonException;
+import exception.TeacherException;
 import model.Class;
 import model.Course;
 import model.Teacher;
+import model.datatype.CPF;
 import model.datatype.Date;
 
 public class ClassDAO extends DAO {
@@ -163,5 +172,141 @@ public class ClassDAO extends DAO {
 			throw new ClassException(INVALID_STATUS);
 		}
 	}
+	/**
+	 * Get a class by the code(classId)
+	 * @param classId
+	 * @throws DateException 
+	 * @throws PersonException 
+	 * @throws TeacherException 
+	 * @throws CPFException 
+	 * @throws ClassException
+	 * @return Class
+	 */
+	public Class getClassByCode(int classId) throws ClassException, CPFException, TeacherException, PersonException, DateException{
+		
+		ResultSet resultSet = null;
+		
+		String query = "SELECT * FROM " + CLASS_TABLE_NAME + " WHERE " + ID_CLASS_COLUMN + " = " + classId ;
+		
+		Class cls = null;
+		
+		try {
+			resultSet = this.search(query);
+			
+			if (!resultSet.next()){
+				throw new ClassException(COULDNT_FIND_CLASS);
+			} 
+			else {
+				while(resultSet.next()){
+					
+					cls = getClassByResultSet(resultSet);
+									
+				}
+			}
+		}
+		finally {
+			return cls;
+		}		
+	}
+	/**
+	 * 
+	 * @param courseId
+	 * @param teacherCPF
+	 * @param shift
+	 * @return
+	 */
+	public ArrayList<Class> getClass(Integer courseId, String teacherCPF, String shift){
+		
+		String query = null;
+		
+		if (courseId != null && teacherCPF != null && shift != null){
+			query = "SELECT * FROM " + CLASS_TABLE_NAME + " WHERE " + ID_COURSE_COLUMN + " = "+ courseId + " AND " + 
+							TEACHER_CPF_COLUMN + " = " + teacherCPF + " AND " + SHIFT_COLUMN + " = " + shift;
+		} else {
+			if (courseId != null && teacherCPF != null){
+				query = "SELECT * FROM " + CLASS_TABLE_NAME + " WHERE " + 
+								ID_COURSE_COLUMN + " = "+ courseId + " AND " + 
+								TEACHER_CPF_COLUMN + " = " + teacherCPF;
+			} else if(courseId != null && shift != null) {
+				query = "SELECT * FROM " + CLASS_TABLE_NAME + " WHERE " + 
+								ID_COURSE_COLUMN + " = "+ courseId + " AND " + 
+								SHIFT_COLUMN + " = " + shift;
+			} else if(teacherCPF != null && shift != null) {
+				query = "SELECT * FROM " + CLASS_TABLE_NAME + " WHERE " + 
+								TEACHER_CPF_COLUMN + " = " + teacherCPF + " AND " + 
+								SHIFT_COLUMN + " = " + shift;
+			} else {
+				if(courseId != null){
+					query = "SELECT * FROM " + CLASS_TABLE_NAME + " WHERE " + 
+									ID_COURSE_COLUMN + " = "+ courseId;
+				} else if (teacherCPF != null){
+					query = "SELECT * FROM " + CLASS_TABLE_NAME + " WHERE " + 
+									TEACHER_CPF_COLUMN + " = " + teacherCPF;
+				} else {
+					query = "SELECT * FROM " + CLASS_TABLE_NAME + " WHERE " + 
+									SHIFT_COLUMN + " = " + shift;
+				}
+			}
+			
+		}
+		
+		ResultSet resultSet = null;
+		ArrayList<Class> classes = null;
+		
+		try {
+			resultSet = this.search(query);
+			
+			if (!resultSet.next()){
+				throw new ClassException(COULDNT_FIND_CLASS);
+			} 
+			else {
+				while(resultSet.next()){
+					classes.add(getClassByResultSet(resultSet));
+				}
+			}
+		} finally {
+			
+			return classes;
+		}
+		
+	}
+	
+	/**
+	 * Get a Class Object from a valid resulSet
+	 * @param resultSet
+	 * @return
+	 * @throws PersonException 
+	 * @throws TeacherException 
+	 * @throws CPFException 
+	 * @throws ClassException 
+	 * @throws SQLException 
+	 * @throws DateException 
+	 */
+	public Class getClassByResultSet(ResultSet resultSet) throws ClassException, CPFException, TeacherException, PersonException, SQLException, DateException{
+		
+		String classId_ = resultSet.getString(ID_CLASS_COLUMN);
+		int courseId = resultSet.getInt(ID_COURSE_COLUMN);
+		String teacherCPF = resultSet.getString(TEACHER_CPF_COLUMN);
+		Date classOpened = new Date(resultSet.getString(OPEN_CLASS));
+		Date classClosed = new Date(resultSet.getString(CLOSED_CLASS));
+		String shift = resultSet.getString(SHIFT_COLUMN);
+		Integer classStatus = resultSet.getInt(STATUS_COLUMN);
+		
+		Class cls = new Class(classId_, classOpened, classClosed, shift, getTeacher(teacherCPF), getCourse(courseId), classStatus);
+		
+		return cls;
+	}
+	
+	public Teacher getTeacher(String cpf) throws CPFException, TeacherException, PersonException{
+		CPF teacherCpf = new CPF(cpf);
+		TeacherController teacherController = new TeacherController();
+		return teacherController.getTeacher(teacherCpf);
+	}
+	
+	public Course getCourse(int courseId){
+		CourseController courseController = new CourseController();
+		return courseController.get(courseId);
+	}
+	
 
 }
